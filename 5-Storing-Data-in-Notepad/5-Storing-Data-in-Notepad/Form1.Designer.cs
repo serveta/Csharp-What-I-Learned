ceef0b53586cf1c8a58f29e6e8ae2d33d6aba2ae
Servet Arslan <arslannservett@gmail.com>
2021-11-05 16:51:50 +0300

    5-Storing Data in Notepad
    Lesson 5

---
+++ 5-Storing-Data-in-Notepad/5-Storing-Data-in-Notepad/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace _5_Storing_Data_in_Notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxName = new System.Windows.Forms.ListBox();
+            this.listBoxPhoneNum = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNum = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxName
+            // 
+            this.listBoxName.FormattingEnabled = true;
+            this.listBoxName.Location = new System.Drawing.Point(94, 26);
+            this.listBoxName.Name = "listBoxName";
+            this.listBoxName.Size = new System.Drawing.Size(168, 212);
+            this.listBoxName.TabIndex = 0;
+            this.listBoxName.SelectedIndexChanged += new System.EventHandler(this.listBoxName_SelectedIndexChanged);
+            // 
+            // listBoxPhoneNum
+            // 
+            this.listBoxPhoneNum.FormattingEnabled = true;
+            this.listBoxPhoneNum.Location = new System.Drawing.Point(329, 26);
+            this.listBoxPhoneNum.Name = "listBoxPhoneNum";
+            this.listBoxPhoneNum.Size = new System.Drawing.Size(168, 212);
+            this.listBoxPhoneNum.TabIndex = 1;
+            this.listBoxPhoneNum.SelectedIndexChanged += new System.EventHandler(this.listBoxPhoneNum_SelectedIndexChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(162, 271);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            this.txtName.TextChanged += new System.EventHandler(this.txtNameAndPhoneNum_TextChanged);
+            // 
+            // txtPhoneNum
+            // 
+            this.txtPhoneNum.Location = new System.Drawing.Point(329, 271);
+            this.txtPhoneNum.Name = "txtPhoneNum";
+            this.txtPhoneNum.Size = new System.Drawing.Size(100, 20);
+            this.txtPhoneNum.TabIndex = 3;
+            this.txtPhoneNum.TextChanged += new System.EventHandler(this.txtNameAndPhoneNum_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(187, 309);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(329, 309);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Phone Number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 353);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPhoneNum);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.listBoxPhoneNum);
+            this.Controls.Add(this.listBoxName);
+            this.Name = "Form1";
+            this.Text = "Phone Book App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxName;
+        private System.Windows.Forms.ListBox listBoxPhoneNum;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhoneNum;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 5-Storing-Data-in-Notepad/5-Storing-Data-in-Notepad/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5_Storing_Data_in_Notepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("phoneBook.txt"))
+            {
+                readData();
+            }
+
+            btnAdd.Enabled = false;
+        }
+
+        private void txtNameAndPhoneNum_TextChanged(object sender, EventArgs e)
+        {
+            if (txtName.Text.Length > 1 && txtPhoneNum.Text.Length == 10)
+            {
+                btnAdd.Enabled = true;
+            }
+            else
+            {
+                btnAdd.Enabled = false;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            FileStream fileStream = new FileStream("phoneBook.txt",FileMode.Append);
+            StreamWriter streamWriter = new StreamWriter(fileStream);
+            streamWriter.WriteLine(txtName.Text + "-" + txtPhoneNum.Text);
+            streamWriter.Close();
+            fileStream.Close();
+
+            txtName.ResetText();
+            txtPhoneNum.ResetText();
+            readData();
+        }
+
+        void readData()
+        {
+            listBoxName.Items.Clear();
+            listBoxPhoneNum.Items.Clear();
+
+            string[] data = File.ReadAllLines("phoneBook.txt");
+
+            for (int i = 0; i < data.Length; i++)
+            {
+               
+                char[] ch = {'-'};
+                string[] partOfData = data[i].Split(ch, StringSplitOptions.RemoveEmptyEntries);
+
+                if (partOfData[0].Length>0 && partOfData[1].Length>0)
+                {
+                    listBoxName.Items.Add(partOfData[0].ToString());
+                    listBoxPhoneNum.Items.Add(partOfData[1].ToString());
+                }
+                
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void listBoxName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBoxPhoneNum.SelectedIndex = listBoxName.SelectedIndex;
+        }
+
+        private void listBoxPhoneNum_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBoxName.SelectedIndex = listBoxPhoneNum.SelectedIndex;
+        }
+
+    }
+}
