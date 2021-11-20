4a1b9727c7de9c9a78ba657273ac1869a1e44ac4
Servet Arslan <arslannservett@gmail.com>
2021-11-20 11:42:58 +0300

    Relation between Forms

---
+++ 8-relation-between-forms/8-relation-between-forms/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_relation_between_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Form2 form2;
+        public static int count;
+        public static string txt;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            form2 = new Form2();
+            label1.ResetText();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            form2.ShowDialog();
+            label1.Text = txt;
+        }
+    }
+}
